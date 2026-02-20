57fa9d6d6790aa8e2ed3062d5c0b3b1698c7abe3
Tom Naessens <tom.naessens@dodona.be>
2026-02-20 10:24:57 +0100

    Update extension publisher, name, and displayname

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dodona-exercise-plugin" is now active in the web extension host!');
+	console.log('Congratulations, your extension "dodona-exercise-assistant" is now active in the web extension host!');
 }
 
 // This method is called when your extension is deactivated
